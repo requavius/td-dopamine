d355066166bbd133a1bad79895a5a18abc1a0ea0
Rey <elfikyr2008@gmail.com>
2026-02-25 00:24:57 -0500

    Changed interpretation each stage has a reward. V is the expected performance values closer 1 meaning perfection and values to 0 are poor. A positive D represents doing better than expected while a negative is worse and 0 is exactly as expected.

--- td.py
+++ td.py
@@ -1,34 +1,43 @@
-
+import random
+import math
 
 
 #if skill is too much higher than difficulty no novelty and if skill too much lower no reward
 #skill increases through iterations
 stage_amt = 4
-difficulty = 2
-skill = 2
+difficulty = float(1)
+skill = float(2)
 #if large reward initially that drops off
 disc_fac = .9
-a = 1
+a = .9
 #reward how total enjoyment after iteration
 r = 1
+noise_level = 0.5
 
-def value_of_stage():
-    stage = {s: {"V": 0.0, "R": 0.0} for s in range(stage_amt)}
+def value_of_stage(difficulty, skill, epochs = 1):
+    stage = {s: {"V": 0.0, "D": 0.0} for s in range(stage_amt)}
+    for _ in range(epochs):
+        for s in range(stage_amt): 
+            oldV = {s: stage[s]["V"]} 
+            epsilon = random.uniform(-noise_level, noise_level)
+            
+            obs = 1 / math.e**-(skill - difficulty) + epsilon
+            obs = max(0, min(1, obs))
 
-    for _ in range(stage_amt): 
-        oldV = {s: stage[s]["V"] for s in stage} 
-
-        for s in range(stage_amt):
-
-            if s == stage_amt - 1:
-                target = r
-            else:
-                target = disc_fac * oldV[s + 1]
-
-            stage[s]["V"] = oldV[s] + a * (target - oldV[s])
-
+            stage[s]["V"] = oldV[s] + a * (obs - oldV[s])
+            stage[s]["D"] = round((obs - oldV[s]), 4)
+            stage[s]["V"] = round(stage[s]["V"], 4)
+            
     return stage
 
-print(value_of_stage())
+def prediction_error(stage_vals):
+    surprise = [
+    stage_vals[s]["R"] + disc_fac * stage_vals[s+1]["V"] - stage_vals[s]["V"]
+    if s < len(stage_vals) - 1
+    else stage_vals[s]["R"] - stage_vals[s]["V"]
+    for s in range(len(stage_vals))
+]
+    return surprise
+print(value_of_stage(4, 1, 3))
 
     
