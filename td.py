4ad5a93c16a1be913b26028f3c75d1e259a46644
Rey <elfikyr2008@gmail.com>
2026-02-26 01:18:54 -0500

    Its alive. Now i need to make it so that the amount of times it does the activity is not predetermined and based on it's experiences

--- td.py
+++ td.py
@@ -1,15 +1,11 @@
 import random
 import math
-import itertools
 
 #if skill is too much higher than difficulty no novelty and if skill too much lower no reward
 #skill increases through iterations
 stage_amt = 4
-#if large reward initially that drops off
 disc_fac = .9
 a = .9
-#reward how total enjoyment after iteration
-r = 1
 
 
 stage_dict = {s: {"V": 0.0, "D": 0.0} for s in range(stage_amt)} 
@@ -24,41 +20,51 @@ def value_of_stage(difficulty, skill, stage, epochs = 1, base_sigma = .02, scali
             epsilon = random.gauss(0, sigma)
             
             obs = 1 / (1 + math.e**-(skill - difficulty) ) + epsilon
+
             obs = max(0, min(1, obs))
 
             stage[s]["V"] = oldV[s] + a * (obs - oldV[s])
             stage[s]["D"] = obs - oldV[s]
-            stage[s]["V"] = stage[s]["V"]
+
         stage["Expected Overall"] = sum(stage[s]["V"] for s in range(stage_amt))/stage_amt
         stage["Delta Overall"] = sum(stage[s]["D"] for s in range(stage_amt))/stage_amt
     
         stage["Trained Amount"] += 1
-    # for s in range(stage_amt):
-    #     stage[s]["V"] = round(stage[s]["V"], 2)
-    #     stage[s]["D"] = round(stage[s]["D"], 2)
-    # stage["Expected Overall"], stage["Delta Overall"] = round(stage["Expected Overall"], 2), round(stage["Delta Overall"], 2)
+
     return stage
 
 def bored(stage_vals):
+    stored_drive = 0
     stage_copy = stage_dict
-    diff = 4 / 9 - 1/3
-    i = 0
+    diff = 0
+    skill_learning = 0
     count = 1
-    while True:
-        scores = {i: {"Overall Exp": 0.0, "Overall Delta": 0.0}}
-        skill = 2 / (1 + math.e**-(i) ) - 1
-        result  = stage_vals(diff, skill, stage_copy)
-        scores[i]["Overall Exp"] = result["Expected Overall"]
-        scores[i]["Overall Delta"] = result["Delta Overall"]
-        if scores[i]["Overall Exp"] >= .9:
-            print(stage_copy["Trained Amount"])
-            break
-        i += 1 + scores[i]["Overall Delta"]
-        count += 1
+    i = 0
+    while count < 5:
+        scores = {i: {"Expected Overall": 0.0, "Delta Overall": 0.0}}
+        result  = stage_vals(diff, i, stage_copy)
+        scores[i]["Expected Overall"] = result["Expected Overall"]
+        scores[i]["Delta Overall"] = result["Delta Overall"]
+
+        if scores[i]["Expected Overall"] >= .9:
+            print(f"Trained {stage_copy["Trained Amount"]} time(s) skill level of {skill_learning}")
+            final_test_enjoyment = stage_vals(diff, i, stage_copy)["Delta Overall"]
+            if -0.01 < final_test_enjoyment:
+                diff += 1 + final_test_enjoyment
+                print(f"I'm pretty confident let me try something harder. Diff is now {diff}")
+                if final_test_enjoyment > 0.01:
+                    stored_drive += final_test_enjoyment
+            elif final_test_enjoyment < -0.01:
+                print(f"Guess im not good enough. ")
+            
+            count += 1
+        skill_learning += 1 + scores[i]["Delta Overall"]
+        i += 1
+        
     return scores
     
         
 
-print(bored(value_of_stage))
+bored(value_of_stage)
 #print(value_of_stage(0, 2, stage_dict, 100))
     
